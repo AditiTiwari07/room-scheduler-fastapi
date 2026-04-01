e79d4bee33c2210cee2575053d2947971827d47b
Aditi Tiwari <aditisarla@gmail.com>
2026-04-01 23:10:02 +0100

    Update app.py to template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 import os
@@ -12,7 +12,7 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    return "Room Scheduler is running!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
